65b786d376f56cf384d195402ba9b31b738f4a92
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-07-04 18:25:57 +0100

    amending function so it is random

--- infectious_disease/surveillance_GoingViral_VaccinationCoverage.R
+++ infectious_disease/surveillance_GoingViral_VaccinationCoverage.R
@@ -3,14 +3,14 @@
 # end_cov is the expected final coverage at the end of the time period as a proportion (default is 0.8)
 # nsteps is the number of time points to provide coverage at
 
+# will produce a series of coverages- they are non monotonic but this could reflect reporting revisions over time
+
 
 generate_vaccination_coverage <- function(start_cov=0, end_cov=0.8, nsteps=100){
   # assume vaccination coverage follows logistic growth
-  x0 <- nsteps/2
-  L <- end_cov
-  k <- 10/nsteps
-  
-  x <- 1:nsteps
-  
-  L/(1+exp(-k*(x-x0)))
+  x <- start_cov
+  for(i in 2:nsteps){
+    x[i] <- x[i-1] + rnorm(1, mean = (end_cov-start_cov)/nsteps, sd = 1/nsteps)
+  }
+  x
 }
