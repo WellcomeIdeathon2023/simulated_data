5c0173299320650fa68bc90dda3732f1e6478b03
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-07-07 11:26:25 +0100

    correcting old comments

--- infectious_disease/surveillance_GoingViral_VaccinationCoverage.R
+++ infectious_disease/surveillance_GoingViral_VaccinationCoverage.R
@@ -1,4 +1,4 @@
-## generate cumulative vaccination coverage over a number of time points using a logistic functional form
+## generate cumulative vaccination coverage over a number of time points by sampling from a normal distribution at each time point
 # start_cov is the starting vaccination coverage level as a proportion (default is 0)
 # end_cov is the expected final coverage at the end of the time period as a proportion (default is 0.8)
 # nsteps is the number of time points to provide coverage at
@@ -7,7 +7,7 @@
 
 
 generate_vaccination_coverage <- function(start_cov=0, end_cov=0.8, nsteps=100){
-  # assume vaccination coverage follows logistic growth
+
   x <- start_cov
   for(i in 2:nsteps){
     x[i] <- x[i-1] + rnorm(1, mean = (end_cov-start_cov)/nsteps, sd = 1/nsteps)
